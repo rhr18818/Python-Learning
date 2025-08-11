61c15e1e355cd3c65a72040474e145211d857b01
rhr18818 <rhr14101@gmail.com>
2025-08-11 19:41:09 +0600

    Monizzaman Sir--Task 1

--- image.py
+++ image.py
@@ -2,8 +2,10 @@ import cv2
 import numpy as np
 
 img = cv2.imread('demo.jpg')
-cv2.imshow("image",img)
+
+cv2.imshow("Original Image",img)
 cv2.waitKey(0)
+
 print(img.shape[:2])
 
 H ,W = img.shape[:2]
@@ -11,7 +13,6 @@ new_H,new_W = H//4,W//4;
 new_img = np.zeros((new_H,new_W,3),dtype=np.uint8)
 print(new_img.shape)
 
-# img = img/255
 
 print(type(img))
 
@@ -22,10 +23,11 @@ for i in range(0,H,4):
         new_img[i//4,j//4] = avg
 
 
-cv2.imshow("New Image",new_img)
+cv2.imshow("Transformed Image!",new_img)
 cv2.waitKey(0)
 
 
+# img = img/255
 # count =0
 # # for i in img:
 # #     count= count +1
